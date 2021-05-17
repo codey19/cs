ecce738cbf2574b4f9bfa863059c66125f382b76
Bob Sheng <bfsheng@live.com>
2021-05-16 20:45:16 -0400

    init

---
+++ algorithm/codey/list/Queue.java
@@ -0,0 +1,68 @@
+package algorithm.codey.list;
+
+public class Queue {
+
+    public LinkedList queue;
+    public int size;
+    static final int DEFAULT_MAX_SIZE = Integer.MAX_VALUE;
+    public int maxSize;
+
+    public Queue() {
+        this(DEFAULT_MAX_SIZE);
+    }
+
+    public Queue(int maxSize) {
+        this.queue = new LinkedList();
+        this.size = 0;
+        this.maxSize = maxSize;
+    }
+
+    public boolean hasSpace() {
+        return this.size < this.maxSize;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    public void enqueue(String data) {
+        if (this.hasSpace()) {
+            this.queue.addToTail(data);
+            this.size++;
+        } else {
+            throw new Error("Queue is full!");
+        }
+    }
+
+    public String dequeue() {
+        if (!this.isEmpty()) {
+            String data = this.queue.removeHead();
+            this.size--;
+            return data;
+        } else {
+            throw new Error("Queue is empty!");
+        }
+    }
+
+    public String peek() {
+        if (this.isEmpty()) {
+            return null;
+        } else {
+            return this.queue.head.data;
+        }
+    }
+
+    public static void main(String[] args){
+        Queue queue = new Queue(2);
+        System.out.println(queue.isEmpty());
+        queue.enqueue("item1");
+        queue.enqueue("item2");
+        try {
+            queue.enqueue("item3");
+        } catch (Error e)  {
+            System.out.println(e.toString());
+        }
+        queue.dequeue();
+
+    }
+}
